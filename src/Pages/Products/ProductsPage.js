28ffa93ace3a81a71eab019f6a261ca2bff2ebac
yelab07 <79489573+yelab07@users.noreply.github.com>
2021-10-20 09:28:48 -0500

    update on productspage

--- src/Pages/Products/ProductsPage.js
+++ src/Pages/Products/ProductsPage.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
 import "./Products.css";
+import "../../App.css";
 const ProductsPage = () => {
   const [products, setProducts] = useState([]);
 
@@ -11,22 +12,36 @@ const ProductsPage = () => {
 
     setProducts(data);
   };
-     console.log(products);
+  console.log(products);
 
   useEffect(() => {
     getproducts();
   }, []);
+  const productList = products.map((prod, index) => {
+    const { image, title, price, id } = prod;
+    return (
+        
+          <div
+            className="product-contaier"
+            // onClick={() => {
+            //   props.setproductSelected(imdbID);
 
-  // fetch("https://fakestoreapi.com/products/1")
-  //   .then((res) => res.json())
-  //   .then((json) => console.log(json));
+            // }}
+          >
+            <img className="product-poster" src={image} alt={title} />
+            {/* <div className="product-name">{prod.title}</div> */}
+            <div className="product-info">
+              <span className="info">Title:{title}</span>
+              <span className="info">Price: ${price}</span>
+            </div>
+         
+        
+      </div>
+    );
+    
+  });
+  return <div className="listed-products">{productList}</div>;
+  
+};
 
-  return (
-    <div>
-      {/* {products} */}
-      <h2>product page</h2>
-    </div>
-  );
-}
-
-export default ProductsPage
+export default ProductsPage;
